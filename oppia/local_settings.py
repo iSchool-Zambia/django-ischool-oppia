419c6bbb2bd564240dacb85eb1e1503b2a72ec32
alexlittle <alex@alexlittle.net>
2015-09-10 15:08:08 +0300

    upda max upload size

--- oppia/local_settings.py
+++ oppia/local_settings.py
@@ -70,7 +70,7 @@ def modify(settings):
     settings['OPPIA_GOOGLE_ANALYTICS_CODE'] = 'UA-3609005-11'
     settings['OPPIA_GOOGLE_ANALYTICS_DOMAIN'] = 'oppia-mobile.org'
     
-    settings['OPPIA_MAX_UPLOAD_SIZE'] = 5242880         # max course file upload size - in bytes
+    settings['OPPIA_MAX_UPLOAD_SIZE'] = 6291456        # max course file upload size - in bytes
     
     settings['API_LIMIT_PER_PAGE'] = 0
     
