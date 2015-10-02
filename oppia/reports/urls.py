acbbf52b894b4288b9dc4304fd9238b564e03261
alexlittle <alex@alexlittle.net>
2015-10-02 17:29:14 +0300

    CLOSED - task 8: User report on how many don't have full user profiles http://github.com/iSchool-Zambia/django-ischool-oppia/issues/issue/8

--- oppia/reports/urls.py
+++ oppia/reports/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-                       
+           
+    url(r'^incomplete-profiles/$', 'oppia.reports.views.incomplete_profiles_view', name="oppia_report_incomplete_profiles"),            
                        
     )
--- oppia/reports/views.py
+++ oppia/reports/views.py
@@ -1,7 +1,24 @@
 # oppia/reports/views.py
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.utils.translation import ugettext_lazy as _
 
+from oppia.models import User, UserProfile
 
 def menu_reports(request):
     # add in here any reports that need to appear in the menu
     #return [{'name': 'test', 'url':'/reports/1/'},{'name': 'test2', 'url':'/reports/2/'}]
-    return None
+    return [
+            { 'name': _(u'Incomplete Profiles'),
+              'url': reverse('oppia_report_incomplete_profiles')}
+            ]
+
+
+def incomplete_profiles_view(request):
+    users = User.objects.filter(userprofile__facility=None,is_staff=False)
+    return render_to_response('oppia/reports/incomplete-profiles.html',
+                              {'users': users,
+                               }, 
+                              context_instance=RequestContext(request))
