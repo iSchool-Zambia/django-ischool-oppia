f2c2deea655903806f4a1226ea0ad1e25d996d8e
alexlittle <alex@alexlittle.net>
2015-11-08 13:01:10 -0500

    models for user locations

---
+++ oppia/profile/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='District',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Facility',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('district', models.ForeignKey(to='profile.District')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Province',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='district',
+            name='province',
+            field=models.ForeignKey(to='profile.Province'),
+        ),
+    ]
--- oppia/profile/models.py
+++ oppia/profile/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Province (models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    
+class District (models.Model):
+    province = models.ForeignKey(Province)
+    name = models.CharField(max_length=200, blank=False, null=False)
+
+class Facility (models.Model):
+    district = models.ForeignKey(District)
+    name = models.CharField(max_length=200, blank=False, null=False)
