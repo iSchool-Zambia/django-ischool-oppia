6ad289d13965850473404f14300cd3fa55a1d797
alexlittle <alex@alexlittle.net>
2016-01-26 12:48:41 +0000

    update search and display fields

--- oppia/profile/admin.py
+++ oppia/profile/admin.py
@@ -12,8 +12,8 @@ class DistrictAdmin(admin.ModelAdmin):
     search_fields = ['name'] 
 
 class FacilityAdmin(admin.ModelAdmin):
-    list_display = ('name',) 
-    search_fields = ['name'] 
+    list_display = ('name', 'code', 'type', 'active') 
+    search_fields = ['name','type','code'] 
     
 admin.site.register(Province, ProvinceAdmin)  
 admin.site.register(District, DistrictAdmin) 
