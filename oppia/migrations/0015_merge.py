4885bf87369282ef60e3da4d87e72352ed3c5b07
alexlittle <alex@alexlittle.net>
2016-03-04 11:32:04 +0200

    merge from core

---
+++ oppia/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('oppia', '0014_auto_20160304_0842'),
+        ('oppia', '0010_move_userprofile'),
+    ]
+
+    operations = [
+    ]
